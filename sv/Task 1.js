f4603a8996ba2ebc0cf8399a014b341eec24f04d
Sathyavarthini <sathyavarthiniravi@gmail.com>
2025-07-20 22:35:14 +0530

    task 1

---
+++ sv/Task 1.js
@@ -0,0 +1,10 @@
+var name="sathya"
+console.log("NAME: ",name);
+name ="varthini";
+console.log("Modified Name",name)
+let age=18;
+console.log("Age: ",age);
+age = 20;
+console.log("Modified Age",age);
+const city="Avinashi";
+console.log("City:",city);
